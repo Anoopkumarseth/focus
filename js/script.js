48cb49e3c46d49937102ac9180fd660e53730a84
Anoop Kumar Seth <anoopkumarseth@hotmail.com>
2022-11-30 00:39:06 +0530

    slider

---
+++ js/script.js
@@ -0,0 +1,30 @@
+body = document.querySelector('body');
+
+siteheader = document.querySelector('.site_header');
+navBtn = siteheader.querySelector('.primaryToggle');
+primaryMenu = siteheader.querySelector('.primary_menu');
+navBtn.addEventListener('click', () => {
+    navBtn.classList.toggle("active")
+    body.classList.toggle("primary_menu_open")
+})
+siteheader.addEventListener("click", () => {
+    navBtn.classList.remove("active")
+    body.classList.remove("primary_menu_open")
+})
+
+function stopProp(element) {
+    element.addEventListener("click", (event) => {
+        event.stopPropagation();
+    })
+};
+stopProp(navBtn)
+stopProp(primaryMenu)
+
+// import Swiper from "swiper";
+// swiper
+var swiper = new Swiper(".testimonial_swiper", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
